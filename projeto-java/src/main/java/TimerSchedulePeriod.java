5a2fd307d9bbbbf8a1836b198003a21a87005637
Matheus <matheus.patricio@sptech.school>
2022-10-07 20:54:14 -0300

    projeto coletando dados e enviando para o banco

---
+++ projeto-java/src/main/java/TimerSchedulePeriod.java
@@ -0,0 +1,22 @@
+
+import java.util.TimerTask;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class TimerSchedulePeriod extends TimerTask {
+
+    public TimerSchedulePeriod() {
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+
+}
