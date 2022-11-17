ccd000f5471bf15381b377fc1768ef04446a8230
Matheus <matheus.patricio@sptech.school>
2022-11-17 13:40:42 -0300

    novas classes adicionadas

---
+++ projeto-java/src/main/java/model/Registro.java
@@ -0,0 +1,12 @@
+package model;
+
+import enums.Alertas;
+
+import java.util.Date;
+
+public class Registro {
+    private Date dataRegistro;
+    private Maquina maquina;
+    private Alertas alerta;
+    
+}
