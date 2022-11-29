0c43fddf1c9c6544dc36a3d353c76c22859bb19c
Matheus <matheus.patricio@sptech.school>
2022-11-28 23:23:39 -0300

    bugfix

--- projeto-java/src/main/java/database/Queries.java
+++ projeto-java/src/main/java/database/Queries.java
@@ -96,13 +96,13 @@ public class Queries {
                 ps.setString(5, alerta);
                 ps.executeUpdate();
                 if (conexaoDocker != null) {
-                    String sql= "insert into registro (id_registro,data_registro,memoria_uso,cpu_uso,disco_uso) values (null,default,?,?,?);";
+                    String sql= "insert into registro (data_registro,memoria_uso,cpu_uso,disco_uso) values (default,?,?,?);";
                     psDock = conexaoDocker.getConDocker().prepareStatement(sql);
                     System.out.println(sql);
                     System.out.println();
-                    psDock.setDouble(2, memoriaUsada);
-                    psDock.setInt(3, cpuUsada);
-                    psDock.setDouble(4, discoUsado);
+                    psDock.setDouble(1, memoriaUsada);
+                    psDock.setInt(2, cpuUsada);
+                    psDock.setDouble(3, discoUsado);
 
                     psDock.executeUpdate();
                 }
